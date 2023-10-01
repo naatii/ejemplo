8a377f34965b5dc7c819f707061fee4d7a42f158
naatii <natiworlds@gmail.com>
2023-10-01 16:47:51 +0200

    he puesto un espacio en un fichero

--- test_ejercicios/test_ejercicios.py
+++ test_ejercicios/test_ejercicios.py
@@ -3,7 +3,7 @@ from ejercicios.ejercicio1 import hola
 def test_hola(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda nombre: "natalia") 
     assert hola() == "hola natalia" # Lanza la comprobación
-
+ 
     # configuramos el test
     # hacemos lo mínimo para que el test funcione
     
